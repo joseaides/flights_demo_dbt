b991e2d161fd98a3e06dfb145339c6e1ec1533a6
joseaides <joseaides@gmail.com>
2025-06-18 10:30:00 +0300

    new fixes

--- models/dim/dim_airport.sql
+++ models/dim/dim_airport.sql
@@ -1,10 +1,7 @@
 {{
 	config(
-		pre_hook="insert into dwh.log_table (id, start_at, model_name) values('{{ invocation_id}}',clock_timestamp(),'{{this.name}}')",
-		post_hook=["insert into {{ this}} select -1, null,null,null,'na',null,'na','na','na','na'"
-                   ,"update  dwh.log_table set end_at=clock_timestamp(), run_time = round(EXTRACT(EPOCH FROM (clock_timestamp() - start_at)) ,2) 
-                     where id = '{{ invocation_id}}' and model_name = '{{this.name}}' "]
-	)
+		post_hook="insert into {{ this}} select -1, null,null,null,'na',null,'na','na','na','na'"
+  )                 
 }}
 
 
--- models/fact/fact_boarding_passes.sql
+++ models/fact/fact_boarding_passes.sql
@@ -1,11 +1,3 @@
-{{
-	config(
-		pre_hook="insert into dwh.log_table (id, start_at, model_name) values('{{ invocation_id}}',clock_timestamp(),'{{this.name}}')",
-		post_hook="update  dwh.log_table set end_at=clock_timestamp(), run_time = round(EXTRACT(EPOCH FROM (clock_timestamp() - start_at)) ,2) 
-                     where id = '{{ invocation_id}}' and model_name = '{{this.name}}' "
-	)
-}}
-
 
 select
     tf.*,
